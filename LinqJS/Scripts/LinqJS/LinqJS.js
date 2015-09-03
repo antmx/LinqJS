3b11236ce21f08d79a70e718a5204ae174a5bb39
antmx <antmx1@ntlworld.com>
2015-09-03 20:27:05 +0100

    Added ForEach

--- LinqJS/Scripts/LinqJS/LinqJS.js
+++ LinqJS/Scripts/LinqJS/LinqJS.js
@@ -6,14 +6,59 @@ Object.prototype.Linqify = function () {
 	if (typeof this.length === 'undefined')
 		throw new Error('Linqify requires an array');
 
-	if (this.___linqified)
+	if (this._linqified)
 		return this;
 
-	this.___linqified = true;
+	this._linqified = true;
 
-	// Add extra methods to the instance
+	// Add extra methods to the INSTANCE
 	Netricity.Utilities.extend(this, { Where: Netricity.LinqJS.Where });
 	Netricity.Utilities.extend(this, { Any: Netricity.LinqJS.Any });
+	Netricity.Utilities.extend(this, { First: Netricity.LinqJS.First });
+	Netricity.Utilities.extend(this, { Last: Netricity.LinqJS.Last });
+	Netricity.Utilities.extend(this, { All: Netricity.LinqJS.All });
+
+	// todo
+	// Where DONE
+	// Any DONE
+	// First DONE
+	// Last DONE
+	// All DONE
+	// Aggregate 
+	// Average
+	// Concat
+	// Contains
+	// Count
+	// DefaultIfEmpty
+	// Distinct
+	// ElementAt
+	// Except
+	// ForEach
+	// GroupBy
+	// GroupJoin
+	// Intersect
+	// Join
+	// LongCount
+	// Max
+	// Min
+	// OrderBy
+	// OrderByDescending
+	// Reverse
+	// Select (projection)
+	// SelectMany (project and flatten)
+	// SequenceEqual
+	// SetValue (not LINQ but useful)
+	// Single
+	// Skip
+	// SkipWhile
+	// Sum
+	// Sum(selector)
+	// Take
+	// TakeWhile
+	// ToLookup ?
+	// Union
+	// Zip
+
 
 	return this;
 };
@@ -23,6 +68,11 @@ Namespace.Create("Netricity.LinqJS");
 Netricity.LinqJS.EnsureLambda = function (lambda) {
 	if (typeof (lambda) !== "function")
 		throw new Error("lambda must be a function");
+};
+
+Netricity.LinqJS.EnsureItems = function (list) {
+	if (list.length == 0)
+		throw new Error("Array must contain at least one item");
 };
 
 Netricity.LinqJS.Where = function (lambda) {
@@ -54,4 +104,80 @@ Netricity.LinqJS.Any = function (lambda) {
 	}
 
 	return false;
+};
+
+Netricity.LinqJS.First = function (lambda) {
+	if (typeof (lambda) === "undefined") {
+
+		Netricity.LinqJS.EnsureItems(this);
+
+		return this[0];
+	}
+
+	Netricity.LinqJS.EnsureLambda(lambda);
+
+	for (var idx = 0; idx < this.length; idx++) {
+		var obj = this[idx];
+
+		if (lambda(obj))
+			return obj;
+	}
+
+	throw new Error("Array contains no matching items");
+};
+
+Netricity.LinqJS.Last = function (lambda) {
+	if (typeof (lambda) === "undefined") {
+
+		Netricity.LinqJS.EnsureItems(this);
+
+		return this[this.length - 1];
+	}
+
+	Netricity.LinqJS.EnsureLambda(lambda);
+
+	for (var idx = this.length - 1; idx >= 0; idx--) {
+		var obj = this[idx];
+
+		if (lambda(obj))
+			return obj;
+	}
+
+	throw new Error("Array contains no matching items");
+};
+
+Netricity.LinqJS.All = function (lambda) {
+	Netricity.LinqJS.EnsureLambda(lambda);
+
+	for (var idx = 0; idx < this.length; idx++) {
+		var obj = this[idx];
+
+		if (!lambda(obj))
+			return false;
+	}
+
+	return true;
+};
+
+Netricity.LinqJS.ForEach = function (lamda) {
+	Netricity.LinqJS.EnsureLambda(lamda);
+
+	for (var idx = 0; idx < this.length; idx++) {
+		var obj = this[idx];
+
+		lambda(obj);
+	}
+};
+
+/// Applies an accumulator function over a sequence.
+Netricity.LinqJS.Aggregate = function (lambda) {
+	Netricity.LinqJS.EnsureLambda(lambda);
+
+	var forLambda = function (lambda) {
+		var result;
+
+		for (var idx = 0; idx < this.length; idx++) {
+
+		}
+	}
 };
--- LinqJS/Scripts/LinqJS/Tests.js
+++ LinqJS/Scripts/LinqJS/Tests.js
@@ -1,13 +1,23 @@
 ﻿
 /// <reference path="~/Scripts/LinqJS/LinqJS.js" />
 
-var a = [1, 2];
+var a = [1, 2, 3, 4];
 
 // Where
-console.log(a.Linqify().Where(function (i) {
-	return i % 2 == 0;
-}));
+console.log(a.Linqify().Where(function (i) { return i % 2 == 0; }));
 
 // Any
 console.log(a.Linqify().Any());
 console.log(a.Linqify().Any(function (i) { return i % 3 == 0; }));
+
+// First
+console.log(a.Linqify().First());
+console.log(a.Linqify().First(function (i) { return i % 2 == 0; }));
+
+// Last
+console.log(a.Linqify().Last());
+console.log(a.Linqify().Last(function (i) { return i % 2 == 0; }));
+
+// All
+console.log(a.Linqify().All(function (i) { return i % 2 == 0; }));
+console.log(a.Linqify().All(function (i) { return i < 100; }));
