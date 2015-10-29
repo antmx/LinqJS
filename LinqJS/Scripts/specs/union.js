91757701026dfce22a2cbdd82804f2c30796dea0
antmx <antmx1@ntlworld.com>
2015-10-29 21:14:23 +0000

    Added union comparer test

--- LinqJS/Scripts/specs/union.js
+++ LinqJS/Scripts/specs/union.js
@@ -3,20 +3,28 @@
 describe("LinqHelper union", function () {
 
 	var helper;
-	var firstItems;
-	var secondItems;
 
 	beforeEach(function () {
 		helper = new Netricity.LinqJS.LinqHelper();
-
-		firstItems = [1, 1, 2, 3, 4, 5, 5, 5];
-		secondItems = [1, 3, 5, 7, 9, 5, 5, 5, 11, 11, 11, 11, 11];
 	});
 
 	it("Produces the set union of two lists, i.e. a list that contains the elements from both lists, excluding duplicates.", function () {
 
+		var firstItems = [1, 1, 2, 3, 4, 5, 5, 5];
+		var secondItems = [1, 3, 5, 7, 9, 5, 5, 5, 11, 11, 11, 11, 11];
 		var result = helper.union(firstItems, secondItems);
 		var expected = [1, 2, 3, 4, 5, 7, 9, 11];
+
+		expect(result).toEqual(expected);
+	});
+
+	it("Produces the set union of two lists, i.e. a list that contains the elements from both lists, excluding duplicates, using the specified equality comparer function.", function () {
+
+		var firstItems = ["One", "one", "Two", "Three", "Four", "Five", "five", "five"];
+		var secondItems = ["one", "three", "five", "Seven", "Nine", "five", "Five", "five", "Eleven", "eleven", "eleven", "eleven"];
+		var comparerLambda = function (first, second) { return first.toLowerCase() == second.toLowerCase(); };
+		var result = helper.union(firstItems, secondItems, comparerLambda);
+		var expected = ["One", "Two", "Three", "Four", "Five", "Seven", "Nine", "Eleven"];
 
 		expect(result).toEqual(expected);
 	});
