908d4ad54a0b520aa8eecff495c6f674466fd9b1
Anthony Chambers <chambersa@eip.tech>
2024-01-17 15:07:47 +0000

    Added linqArray class and test page

---
+++ LinqJS/Scripts/linqArray/linqArray-test.js
@@ -0,0 +1,45 @@
+﻿
+let arr1 = new linqArray();
+arr1[0] = "foo";
+arr1[1] = "bar";
+arr1[2] = "baz";
+
+let result1 = arr1.where((itm) => itm.endsWith("z"));
+
+console.log(...result1);
+
+
+
+let arr2 = new linqArray(["foo", "bar", "baz"]);
+//let arr2 = new linqArray(null);
+
+let result2 = arr2.where((itm) => itm.endsWith("z"));
+
+console.log(...result2);
+
+
+
+let arr3 = new linqArray(["foo", "bar", "baz"]);
+
+let result3 = arr3.where((itm) => itm.endsWith("z")).first();
+
+console.log(result3);
+
+
+
+let arr4 = new linqArray(["foo", "bar", "baz"]);
+
+let result4 = arr4
+    .where((itm) => itm.indexOf("a") > -1)
+    .first();
+
+console.log(result4);
+
+
+
+let arr5 = new linqArray(["foo", "bar", "baz"]);
+
+let result5 = arr5
+    .first((itm) => itm.startsWith("b"));
+
+console.log(result5);
---
+++ LinqJS/Scripts/linqArray/linqArray.js
@@ -0,0 +1,112 @@
+﻿/**
+ * An array with Linq functions.
+ */
+class linqArray extends Array {
+
+    /**
+     * @param {[]} [items] An optional array of items to add to the new linqArray instance
+     */
+    constructor(items) {
+        if (Object.prototype.toString.call(items) === '[object Array]') {
+            super(...items);
+        }
+        else {
+            super();
+        }
+    }
+
+    //#region Private methods
+
+    #isArray(arr) {
+        return Object.prototype.toString.call(arr) === '[object Array]';
+    }
+
+    #isFunction(fn) {
+        return Object.prototype.toString.call(fn) === '[object Function]';
+    }
+
+    #ensureFunc(possibleFunc) {
+
+        if (!this.#isFunction(possibleFunc)) {
+            throw new Error("possibleFunc must be a function");
+        }
+    }
+
+    /**
+     * Checks the specified object is an array containing at least 1 item
+     * @param {ArrayLike} list
+     * @param {boolean} canBeEmpty
+     * @returns {any}
+     */
+    #ensureItems(list, canBeEmpty) {
+
+        if (list == null) {
+            throw new Error("Array must not be null");
+        }
+
+        if (!this.#isArray(list)) {
+            throw new Error("list must be an array");
+        }
+
+        if (list.length === 0 && canBeEmpty !== true) {
+            throw new Error("Array must contain at least one item");
+        }
+
+        return true;
+    };
+
+    //#endregion
+
+    /**
+     * 
+     * @param {[]} items
+     */
+    addItems(items) {
+        items.forEach(item => this.push(item));
+    }
+
+    /**
+     * Filters a sequence of values based on a predicate.
+     * @param {Function} predicateFn
+     * @returns
+     */
+    where(predicateFn) {
+
+        this.#ensureFunc(predicateFn);
+
+        var result = new linqArray();
+
+        this.forEach(item => {
+            if (predicateFn(item)) {
+                result.push(item);
+            }
+        });
+
+        return result;
+    }
+
+    /**
+     * Returns the first element of a sequence.
+     * @param {Function=} predicateFn
+     * @returns The first matching item
+     */
+    first(predicateFn) {
+
+        if (predicateFn === undefined) {
+            this.#ensureItems(this);
+            return this[0];
+        }
+
+        this.#ensureFunc(predicateFn);
+
+        for (var idx = 0, itm; idx < this.length; idx += 1) {
+            itm = this[idx];
+
+            if (predicateFn(itm)) {
+                return itm;
+            }
+        }
+
+        throw new Error("Array contains no matching items");
+    }
+}
